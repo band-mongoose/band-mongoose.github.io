e594019c5c58fa4c6d76c770dcf4720eedd26f26
Alan(Minkyu Cho) <mrnoname@coupang.com>
2014-07-13 23:40:57 +0900

    Change Skin

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,170 @@
-var mongoose = angular.module('mongoose', ['ngRoute']);
-
-mongoose.config(['$routeProvider',
-	function ($routeProvider) {
-		$routeProvider.
-			when('/', {
-				templateUrl: 'assets/templates/home.html'
-			}).
-			when('/about', {
-				templateUrl: 'assets/templates/about.html'
-			}).
-			when('/photo', {
-				templateUrl: 'assets/templates/photo.html',
-				controller: 'PhotoController'
-			}).
-			when('/discography', {
-				templateUrl: 'assets/templates/discography.html'
-			}).
-			when('/record', {
-				templateUrl: 'assets/templates/record.html'
-			}).
-			otherwise({
-				redirectTo: '/'
-			});
-	}]);
-
-//mongoose.controller('TalkController', function($scope) {
-//});
-
-mongoose.controller('PhotoController', function ($scope) {
-	$("#gallery").jsFlickrGallery(
-		{
-			'structure': {
-				'liClass': '.col-md-2'
-			},
-			'modal': {
-				'generate': false
-			}
+var mongoose = angular.module('mongoose', [ 'ngRoute' ]);
+mongoose
+.config(
+		['$routeProvider', '$locationProvider',
+				function($routeProvider, $locationProvider) {
+					$locationProvider.html5Mode(false).hashPrefix('!');
+					$routeProvider.when('/home', {
+						templateUrl : 'assets/templates/home.html',
+						controller : 'HomeController'
+					}).when('/home/:id', {
+						templateUrl : 'assets/templates/home.html',
+						controller : 'HomeController'
+					}).when('/music', {
+						templateUrl : 'assets/templates/music.html'
+					}).when('/photos', {
+						templateUrl : 'assets/templates/photo.html',
+						controller : 'PhotoController'
+					}).when('/videos', {
+						templateUrl : 'assets/templates/discography.html'
+					}).when('/fans', {
+						templateUrl : 'assets/templates/fans.html',
+						controller : 'FansController'
+					}).otherwise({
+						redirectTo : '/home'
+					});
+				} ]).run(function($location) {
+	//jQuery to collapse the navbar on scroll
+	$(window).scroll(function() {
+		if($(".navbar").offset().top > 50){
+			$(".navbar-fixed-top").addClass("top-nav-collapse");
+		}else{
+			$(".navbar-fixed-top").removeClass("top-nav-collapse");
 		}
-	);
+	});
 });
 
+mongoose.controller('HomeController',
+		function($scope, $location, $routeParams) {
+			var id = '#' + (angular.isUndefined($routeParams.id) ? 'page-top'
+					: $routeParams.id);
+			$('html, body').stop().animate({
+				scrollTop : $(id).offset().top
+			}, 1500, 'easeInOutExpo');
+		});
+
+mongoose.controller('PhotoController', function($scope) {
+});
+
+mongoose.controller('FansController', function($scope) {
+});
+//Google Map Skin - Get more at http://snazzymaps.com/
+//var myOptions = {
+//	zoom : 15,
+//	center : new google.maps.LatLng(53.385873, -1.471471),
+//	mapTypeId : google.maps.MapTypeId.ROADMAP,
+//	disableDefaultUI : true,
+//	styles : [ {
+//		"featureType" : "water",
+//		"elementType" : "geometry",
+//		"stylers" : [ {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 17
+//		} ]
+//	}, {
+//		"featureType" : "landscape",
+//		"elementType" : "geometry",
+//		"stylers" : [ {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 20
+//		} ]
+//	}, {
+//		"featureType" : "road.highway",
+//		"elementType" : "geometry.fill",
+//		"stylers" : [ {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 17
+//		} ]
+//	}, {
+//		"featureType" : "road.highway",
+//		"elementType" : "geometry.stroke",
+//		"stylers" : [ {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 29
+//		}, {
+//			"weight" : 0.2
+//		} ]
+//	}, {
+//		"featureType" : "road.arterial",
+//		"elementType" : "geometry",
+//		"stylers" : [ {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 18
+//		} ]
+//	}, {
+//		"featureType" : "road.local",
+//		"elementType" : "geometry",
+//		"stylers" : [ {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 16
+//		} ]
+//	}, {
+//		"featureType" : "poi",
+//		"elementType" : "geometry",
+//		"stylers" : [ {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 21
+//		} ]
+//	}, {
+//		"elementType" : "labels.text.stroke",
+//		"stylers" : [ {
+//			"visibility" : "on"
+//		}, {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 16
+//		} ]
+//	}, {
+//		"elementType" : "labels.text.fill",
+//		"stylers" : [ {
+//			"saturation" : 36
+//		}, {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 40
+//		} ]
+//	}, {
+//		"elementType" : "labels.icon",
+//		"stylers" : [ {
+//			"visibility" : "off"
+//		} ]
+//	}, {
+//		"featureType" : "transit",
+//		"elementType" : "geometry",
+//		"stylers" : [ {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 19
+//		} ]
+//	}, {
+//		"featureType" : "administrative",
+//		"elementType" : "geometry.fill",
+//		"stylers" : [ {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 20
+//		} ]
+//	}, {
+//		"featureType" : "administrative",
+//		"elementType" : "geometry.stroke",
+//		"stylers" : [ {
+//			"color" : "#000000"
+//		}, {
+//			"lightness" : 17
+//		}, {
+//			"weight" : 1.2
+//		} ]
+//	} ]
+//};
+
+//var map = new google.maps.Map(document.getElementById('map'), myOptions);
 
 //// global. currently active menu item
 //var current_item = 0;
